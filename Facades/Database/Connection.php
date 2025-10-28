d74f3998e7ffb4f1c2f1deb1dc7220a8836d6794
Caio Felipe <caiofelipesmoreira@gmail.com>
2025-10-28 15:45:44 -0300

    Corrigindo erro de digitação

--- Facades/Database/Connection.php
+++ Facades/Database/Connection.php
@@ -16,7 +16,7 @@ class Connection
     public static function getConnection($dns, $username = null, $password = null, $options = null)
     {
         $key = $dns . $username;
-        $env = defined('APPLICATION_ENV') ? constant('APPLICAION_ENV') : 'production';
+        $env = defined('APPLICATION_ENV') ? constant('APPLICATION_ENV') : 'production';
         if (!isset(self::$connections[$key])) {
             $pdo = new PDO($dns, $username, $password, $options);
             $env === 'production' || $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
